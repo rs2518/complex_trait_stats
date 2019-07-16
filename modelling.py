0b60f3cdd23b25346f00028bf9c08d54c0745593
rs2518 <rs2518@ic.ac.uk>
2019-07-16 11:45:11 +0100

    Fixed plot of model coefficients

--- modelling.py
+++ modelling.py
@@ -9,7 +9,7 @@ Created on Sun Jun  9 20:34:07 2019
 
 # ============= DATA PREPARATION =============== #
 
-# Load dataframes
+# Set directory and Load dataframes
 import numpy as np
 import pandas as pd
 import os
@@ -24,77 +24,86 @@ os.chdir(os.path.expanduser('~'))
 home = 'Desktop/MSc Health Data Analytics - IC/HDA/Term 3 MSc Project/Analysis/GeneAtlas/'
 directory = 'Data/Processed/'
 
-data = pd.read_csv(
-        os.path.join(home, directory) + 'processed_data_sample.csv',
-        index_col = 0)
-int_df = pd.read_csv(
-        os.path.join(home, directory) + 'integrated_data_sample.csv',
-        index_col = 0)
+data = pd.read_pickle(
+        os.path.join(home, directory) + 'processed_data_sample.pkl')
+int_df = pd.read_pickle(
+        os.path.join(home, directory) + 'integrated_data_sample.pkl')
+# NOTE: int_df more suitable for statsmodel
+
+
+# Set inputs/targets then split data into test and train sets
+# (X, y split)
+from sklearn.model_selection import train_test_split
+
+processed_cols = [col for col in data.columns.to_list() if col not in int_df.columns.to_list()]
+
+X = data[processed_cols]
+y = data['p_value']
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 0.3,
+                                                    random_state = 21)
+
+# Log-transformed targets
+log_y_train = -np.log(y_train)
+log_y_test = -np.log(y_test)
+
+
+# (Integrated data split)
+train_data, test_data = train_test_split(data, 
+                                         test_size = 0.3,
+                                         random_state = 21)
+
+train_intdf, test_intdf = train_test_split(int_df, 
+                                         test_size = 0.3,
+                                         random_state = 21)
 
 
 
 # ============= SCIKIT LEARN MODELS =============== #
 
-# Split data into test and train sets
-from sklearn.model_selection import train_test_split
 
+########## LINEAR REGRESSION ##########
 
-# Set input/target values
-processed_cols = [col for col in data.columns.to_list() if col not in int_df.columns.to_list()]
-X = data[processed_cols]
-y = data['p_value']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size = 0.3,
-                                                    random_state = 21)
-
-log_y_train = -np.log(y_train)
-log_y_test = -np.log(y_test)
-
-
-########## LINEAR MODEL ##########
-
-# Fit model and predict on test set. Return metrics
-from sklearn import linear_model
+# Scikit-learn linear regression
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-modela = linear_model.LinearRegression()
-modela.fit(X_train, y_train)
-y_pred = modela.predict(X_test)
+sk_linear = LinearRegression()
+sk_linear.fit(X_train, y_train)
+sk_linear_pred = sk_linear.predict(X_test)
 
-modela_coefs = pd.DataFrame(np.append(modela.intercept_, modela.coef_),
+sk_linear_coefs = pd.DataFrame(np.append(sk_linear.intercept_,
+                                              sk_linear.coef_),
                            index = ['intercept'] +list(X.columns), 
                            columns = ['Coefficients'])
-print('Linear model coefficients :\n', modela_coefs)
-print('Mean-squared error :', mean_squared_error(y_test, y_pred))
-print('Variance explained (R^2) :' , r2_score(y_test, y_pred))
+print('Linear model coefficients :\n', sk_linear_coefs)
+print('Mean-squared error :', mean_squared_error(y_test, sk_linear_pred))
+print('Variance explained (R^2) :' , r2_score(y_test, sk_linear_pred))
 
-
-#plt.scatter(x = X_test.index, y = (y_test - y_pred), c = 'g')
+#plt.scatter(x = X_test.index, y = (y_test - sk_linear_pred), c = 'g')
 #plt.show()     # Plot not useful
-
-sns.distplot((y_test - y_pred))
+sns.distplot((y_test - sk_linear_pred))
 plt.show()     # Residuals should be normally distributed
 
 
-##### Log transformed p-values
 
-modelb = linear_model.LinearRegression()
-modelb.fit(X_train, log_y_train)
-log_y_pred = modelb.predict(X_test)
+# Log transformed p-values
+log_sk_linear = LinearRegression()
+log_sk_linear.fit(X_train, log_y_train)
+log_sk_lin_pred = log_sk_linear.predict(X_test)
 
-modelb_coefs = pd.DataFrame(np.append(modelb.intercept_, modelb.coef_),
+log_sk_lin_coefs = pd.DataFrame(np.append(log_sk_linear.intercept_,
+                                      log_sk_linear.coef_),
                            index = ['intercept'] +list(X.columns), 
                            columns = ['Coefficients'])
-print('Linear model coefficients (log-transformed) :\n', modelb_coefs)
-print('Mean-squared error :', mean_squared_error(log_y_test, log_y_pred))
-print('Variance explained (R^2) :', r2_score(log_y_test, log_y_pred))
+print('Linear model coefficients (log-transformed) :\n', log_sk_lin_coefs)
+print('Mean-squared error :', mean_squared_error(log_y_test, log_sk_lin_pred))
+print('Variance explained (R^2) :', r2_score(log_y_test, log_sk_lin_pred))
 
 
-#plt.scatter(x = X_test.index, y = (log_y_test - log_y_pred), c = 'g')
+#plt.scatter(x = X_test.index, y = (log_y_test - log_sk_lin_pred), c = 'g')
 #plt.show()     # Plot not useful
-
-sns.distplot((log_y_test - log_y_pred))
+sns.distplot((log_y_test - log_sk_lin_pred))
 plt.show()     # Residuals should be normally distributed
 
 
@@ -108,12 +117,14 @@ plt.show()     # Residuals should be normally distributed
 # ADDITIONAL SUPPORT FEATURES (STORING SCORES WITHOUT LOOPS)
 
 
-# Fit model and predict on test set. Return metrics
-folds = 5
+# Set folds for crossvalidation
+from sklearn.linear_model import Lasso
 from sklearn.model_selection import GridSearchCV
+folds = 5
 
-# Compute lasso model and plot coefficients
-lasso = linear_model.Lasso(normalize = False)
+
+# Scikit-learn LASSO regression
+lasso = Lasso(normalize = False)
 l1_space = np.logspace(-10, 10, 21)
 l1_param = {'alpha': l1_space}
 
@@ -121,8 +132,6 @@ lasso_cv = GridSearchCV(lasso, l1_param, cv = folds)
 lasso_cv.fit(X_train, y_train)
 lasso_pred = lasso_cv.predict(X_test)
 
-
-# Compute metrics and plots
 print('Best LASSO (l1) penalty :\n', lasso_cv.best_params_)
 print('Best model coefficients :\n', lasso_cv.best_estimator_.coef_)
 print('Mean-squared error :', mean_squared_error(y_test, lasso_pred))
@@ -133,6 +142,8 @@ plt.plot(range(len(X.columns)), lasso_cv.best_estimator_.coef_)
 plt.xticks(range(len(X.columns)), X.columns.values, rotation=90)
 plt.show()
 
+
+plt.figure()
 lasso_scores = lasso_cv.cv_results_['mean_test_score']
 lasso_se = lasso_cv.cv_results_['std_test_score']
 plt.semilogx(l1_space, lasso_scores)
@@ -147,10 +158,9 @@ plt.xlim([l1_space[0], l1_space[-1]])
 plt.show()
 
 
-##### Log transformed p-values
 
-# Compute log_lasso model and plot coefficients
-log_lasso = linear_model.Lasso(normalize = False)
+# Log transformed p-values
+log_lasso = Lasso(normalize = False)
 log_lasso_cv = GridSearchCV(log_lasso, l1_param, cv = folds)
 log_lasso_cv.fit(X_train, log_y_train)
 log_lasso_pred = log_lasso_cv.predict(X_test)
@@ -182,8 +192,10 @@ plt.show()
 
 ########## RIDGE REGRESSION ##########
 
-# Compute ridge model and plot coefficients
-ridge = linear_model.Ridge(normalize = False)
+# Scikit-learn Ridge regression
+from sklearn.linear_model import Ridge
+
+ridge = Ridge(normalize = False)
 l2_space = np.logspace(-10, 10, 21)
 l2_param = {'alpha': l2_space}
 
@@ -191,8 +203,6 @@ ridge_cv = GridSearchCV(ridge, l2_param, cv = folds)
 ridge_cv.fit(X_train, y_train)
 ridge_pred = ridge_cv.predict(X_test)
 
-
-# Compute metrics and plots
 print('Best Ridge (l2) penalty :\n', ridge_cv.best_params_)
 print('Best model coefficients :\n', ridge_cv.best_estimator_.coef_)
 print('Mean-squared error :', mean_squared_error(y_test, ridge_pred))
@@ -217,10 +227,9 @@ plt.xlim([l2_space[0], l2_space[-1]])
 plt.show()
 
 
-##### Log transformed p-values
 
-# Compute log_ridge model and plot coefficients
-log_ridge = linear_model.Ridge(normalize = False)
+# Log transformed p-values
+log_ridge = Ridge(normalize = False)
 log_ridge_cv = GridSearchCV(log_ridge, l2_param, cv = folds)
 log_ridge_cv.fit(X_train, log_y_train)
 log_ridge_pred = log_ridge_cv.predict(X_test)
@@ -252,8 +261,10 @@ plt.show()
 
 ########## ELASTIC NET ##########
 
-# Compute Elastic net model and plot coefficients
-enet = linear_model.ElasticNet(normalize = False)
+# Scikit-learn Elastic Net regression
+from sklearn.linear_model import ElasticNet
+
+enet = ElasticNet(normalize = False)
 l1_ratios = np.linspace(0, 1, 21)
 enet_param = {'l1_ratio': l1_ratios}
 
@@ -261,8 +272,6 @@ enet_cv = GridSearchCV(enet, enet_param, cv = folds)
 enet_cv.fit(X_train, y_train)
 enet_pred = enet_cv.predict(X_test)
 
-
-# Compute metrics and plots
 print('Best Elastic Net penalty (l1 ratio) :\n', enet_cv.best_params_)
 print('Best model coefficients :\n', enet_cv.best_estimator_.coef_)
 print('Mean-squared error :', mean_squared_error(y_test, enet_pred))
@@ -287,10 +296,9 @@ plt.xlim([l1_ratios[0], l1_ratios[-1]])
 plt.show()
 
 
-##### Log transformed p-values
 
-# Compute log_enet model and plot coefficients
-log_enet = linear_model.ElasticNet(normalize = False)
+# Log transformed p-values
+log_enet = ElasticNet(normalize = False)
 log_enet_cv = GridSearchCV(log_enet, enet_param, cv = folds)
 log_enet_cv.fit(X_train, log_y_train)
 log_enet_pred = log_enet_cv.predict(X_test)
@@ -322,34 +330,26 @@ plt.show()
 
 # ============= STATSMODEL =============== #
 
-# Split data into test and train sets
-train_data, test_data = train_test_split(data, 
-                                         test_size = 0.3,
-                                         random_state = 21)
+
+########## LINEAR REGRESSION (METHOD 1) ##########
+# Categorical variables preprocessed as binary variables
 
 
-from statsmodels.formula.api import glm
+# Statsmodels linear regression glm.Gaussian
+from statsmodels.formula.api import glm, ols
 import statsmodels.api as sm
 
 formula = 'p_value ~' + processed_cols[0]
 for index in range(1, len(processed_cols)):
     formula = formula + '+' + str(processed_cols[index])
 
-## Test C() function in glm on unprocessed categorical variables!!!
-#
-#formula = y_train.name + '~' + processed_cols[0]
-#for index in range(1, len(processed_cols)):
-#    if data[processed_cols[index]].dtype != np.float:
-#        formula = formula + '+C(' + str(processed_cols[index] + ')')
-#    else:
-#        formula = formula + '+' + str(processed_cols[index])
-
-glma = glm(formula = formula, 
+glm_linear = glm(formula = formula, 
           data = train_data, 
           family = sm.families.Gaussian()).fit()
 
-print('Linear model coefficients :\n',glma.params)
-print('Summary :\n',glma.summary())
+print('Linear model coefficients :\n',glm_linear.params)
+print('Summary :\n',glm_linear.summary())
+
 
 
 # Log transformed p-values
@@ -357,29 +357,75 @@ log_formula = 'log_p_val ~' + processed_cols[0]
 for index in range(1, len(processed_cols)):
     log_formula = log_formula + '+' + str(processed_cols[index])
 
-glmb = glm(formula = log_formula, 
+log_glm_linear = glm(formula = log_formula, 
           data = train_data, 
           family = sm.families.Gaussian()).fit()
 
-print('Linear model coefficients :\n',glmb.params)
-print('Summary :\n',glmb.summary())
-
+print('Linear model coefficients :\n',log_glm_linear.params)
+print('Summary :\n',log_glm_linear.summary())
 
 # INTERCEPT IS NOT THE SAME AS IN SCIKIT LEARN?!?!
+# BECAUSE OF DIFFERENT REFERENCES
+
+########## LINEAR REGRESSION (METHOD 2) ##########
+# Categorical variables not preprocessed
+
+# NOTE: Method 2 does NOT work with glm function (PerfectSeparation error)
+
+# Statsmodels linear regression ols
+input_cols = [col for col in int_df.columns.to_list() if col not in ['SNP',
+              'Allele', 'A1', 'A2', 'p_value', 'log_p_val', 'Position']]
+
+formula2 = 'p_value ~' + input_cols[0]
+for index in range(1, len(input_cols)):
+    if int_df[input_cols[index]].dtype != np.float:
+        formula2 = formula2 + '+C(' + str(input_cols[index] + ')')
+    else:
+        formula2 = formula2 + '+' + str(input_cols[index])
+
+ols_linear = ols(formula = formula2, data = train_intdf).fit()
+
+print('Linear model coefficients :\n', ols_linear.params)
+print('Summary :\n', ols_linear.summary())
 
 
-# ============= NEURAL NETWORK =============== #
 
-# Keras neural network
-from keras.layers import Input, Dense, Embedding, Flatten
-from keras.models import Model
-from keras.utils import plot_model
+# Log transformed p-values
+log_formula2 = 'log_p_val ~' + input_cols[0]
+for index in range(1, len(input_cols)):
+    if int_df[input_cols[index]].dtype != np.float:
+        log_formula2 = log_formula2 + '+C(' + str(input_cols[index] + ')')
+    else:
+        log_formula2 = log_formula2 + '+' + str(input_cols[index])
 
+log_ols_linear = ols(formula = log_formula2, data = train_intdf).fit()
+
+print('Linear model coefficients :\n', log_ols_linear.params)
+print('Summary :\n', log_ols_linear.summary())
+
+
+
+# ============= KERAS (NEURAL NETWORK) =============== #
 
 # Create layers
+from keras.layers import Input, Dense, Embedding, Flatten
+from keras.models import Sequential, Model
+
+from keras.utils import plot_model
+
+# Categorical embeddings
+no_positions = np.unique(int_df['Position']).shape[0]
+no_chr = np.unique(int_df['Chr_no']).shape[0]
+
+position_lookup = Embedding(input_dim = no_positions,
+                        output_dim = 1,
+                        input_length = 1,
+                        name = 'Team-Strength')
+
+
+
 input_tensor = Input(shape=(1,))
 output_tensor = Dense(1)(input_tensor)
-
 
 # Build model and plot visual image of model
 nn_model = Model(input_tensor, output_tensor)
