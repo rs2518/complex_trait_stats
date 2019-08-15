1a9dc674b8f7e693eff5ba7ffe7f9e2ad457d8e7
rs2518 <rs2518@ic.ac.uk>
2019-08-15 21:09:07 +0100

    Created file with reused functions for importing

---
+++ Scripts/helper_functions.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 12:55:54 2019
+
+@author: raphaelsinclair
+"""
+
+# ============= LOAD LIBRARIES =============== #
+
+import numpy as np
+import pandas as pd
+import os
+
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.metrics import explained_variance_score as explained_var
+from sklearn.metrics import mean_squared_log_error as MSLE
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import r2_score as R2
+
+
+
+# ============= DATA LOADER =============== #
+
+# Loads specified file given filname and directory
+def pkl_load(filename, directory):
+        
+    return pd.read_pickle(os.path.join(directory, filename) + '.pkl')
+
+
+
+# ============= METRIC TABLE =============== #
+
+# Create table of regression metrics
+def metrics_table(y_true, y_pred):
+    
+    index = ['Mean Squared Error',
+             'Mean Absolute Error',
+             'Explained Variance',
+             'Mean Squared Log Error',
+             'Median Absolute Error',
+             'R-sqaured']
+    
+    data = {'Score' : [MSE(y_true, y_pred),
+                        MAE(y_true, y_pred),
+                        explained_var(y_true, y_pred),
+                        MSLE(y_true, y_pred),
+                        median_absolute_error(y_true, y_pred),
+                        R2(y_true, y_pred)]}
+    
+    table = pd.DataFrame(data = data, index = index)
+    
+    
+    return table
+
+
+
+# ============= STABILITY ANALYSIS =============== #
