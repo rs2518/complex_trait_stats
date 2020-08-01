17daf7c8f7a2b386023c58b677ac0a951320c8b4
rs2518 <rs2518@ic.ac.uk>
2020-08-01 20:18:01 +0100

    Add helper functions for loading data, plotting coefficients and table of metrics

--- utils.py
+++ utils.py
@@ -1,50 +1,135 @@
 import os
 
-# import numpy as np
+import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.metrics import mean_squared_error as MSE
 from sklearn.metrics import mean_absolute_error as MAE
 from sklearn.metrics import explained_variance_score as explained_var
-from sklearn.metrics import mean_squared_log_error as MSLE
+# from sklearn.metrics import mean_squared_log_error as MSLE
 from sklearn.metrics import median_absolute_error
 from sklearn.metrics import r2_score as R2
 
 
-
-# ============= DATA LOADER =============== #
-
-# Loads specified file given filname and directory
-def pkl_load(filename, directory):
-        
-    return pd.read_pickle(os.path.join(directory, filename) + '.pkl')
+ROOT = "~/Desktop/Term 3 MSc Project/complex_trait_stats"
+RAW_DATA = "snp_raw.csv"
+PROCESSED_DATA = "snp_processed.csv"
 
 
 
-# ============= METRIC TABLE =============== #
-
-# Create table of regression metrics
-def metrics_table(y_true, y_pred):
+def load_dataframe(file):
+    """Load data from ROOT folder
+    """
+    filepath = os.path.join(ROOT, "data", file)
+    data = pd.read_csv(filepath, index_col=1)
     
-    index = ['Mean Squared Error',
+    data.drop(data.columns[0], axis=1, inplace=True)
+    
+    return _set_categories(data)
+
+
+def _set_categories(dataframe):
+    """Convert columns with 'object' dtype to 'category'
+    """
+    cols = dataframe.select_dtypes(include=["object"]).columns
+    for col in cols:
+        dataframe[col] = dataframe[col].astype("category")
+        
+    return dataframe
+
+
+def process_category(data):
+    """Convert categorical into binary predictors.
+    
+    Preprocess data for sklearn models that require data to be numerical
+    """
+    # Binarise all categorical variables
+    processed_data = pd.get_dummies(data)
+    
+    # Move label to last column
+    cols = processed_data.columns.to_list()
+    cols.append(cols.pop(cols.index("p_value")))
+    
+    return processed_data[cols]
+
+
+
+def plot_coefs(coefs, names, conf_int=None, cmap="default"):
+    """Coefficient plot
+    
+    Plot coefficients against their respective names. If 'conf_int' is given,
+    error bars are added to the plot to show 95% confidence interval.
+    
+    Setting cmap to "signif" highlights significant coefficients in red whilst
+    insignificant coefficients are in light grey.
+    Setting cmap to "rainbow" returns each coefficient in different colour.
+    Otherwise, cmap is set to default or if cmap="default" but no confidence
+    interval is given.
+    
+    """
+    # Set errorbars (if given)
+    if conf_int is None:
+        yerr = None
+        title = ""
+    else:
+        yerr = np.abs(conf_int.T)
+        title = " w/ 95% conf. interval"
+        # title = " w/ {:.{a}%} conf. interval".format(
+        #     (1-alpha), a=len(str(1-alpha))-4)
+    
+    
+    if cmap == "default":
+        cmap = None
+    elif cmap == "rainbow":
+        cmap = sns.hls_palette(len(names), l=.55)
+    elif cmap == "signif":
+        if conf_int is None:
+            cmap = None
+        else:
+            sign = np.sign(conf_int)
+            inds = sign[:,0] * sign[:,1] > 0
+            cmap = ["red" if inds[i] else "silver" for i in range(len(names))]
+
+    
+    plt.figure()
+    
+    plt.errorbar(names, coefs, yerr=yerr, ecolor=cmap, ls="none")
+    plt.scatter(names, coefs, c=cmap)
+    plt.xticks(rotation=90)
+    plt.ylabel("Coefficient "+r'$x_i$')
+    plt.title("Coefficent plot" + title)
+    plt.hlines(0, len(names), 0, colors="grey", linestyles="--")
+    
+    # plt.show()
+
+
+
+def metrics(y_true, y_pred, dp=4):
+    """Metrics table
+    
+    Returns a table of the following regression metrics to the given number 
+    of decimal places:
+        - Mean Squared Error
+        - Mean Absolute Error
+        - Explained Variance
+        - Median Absolute Error
+        - R-sqaured
+    """    
+    cols = ['Mean Squared Error',
              'Mean Absolute Error',
              'Explained Variance',
-             'Mean Squared Log Error',
              'Median Absolute Error',
-             'R-sqaured']
+             'R-sqaured']    
+    fns = [MSE, MAE, explained_var, median_absolute_error, R2]
     
-    data = {'Score' : [MSE(y_true, y_pred),
-                        MAE(y_true, y_pred),
-                        explained_var(y_true, y_pred),
-                        MSLE(y_true, y_pred),
-                        median_absolute_error(y_true, y_pred),
-                        R2(y_true, y_pred)]}
-    
-    table = pd.DataFrame(data = data, index = index)
-    
-    
-    return table
+    vals = []
+    text = "{:.{dp}f}"
+    for fn in fns:
+        vals.append(float(text.format(fn(y_true, y_pred), dp=dp)))
+    data = np.array(vals).reshape(1,-1)
+        
+    return pd.DataFrame(data, columns=cols)
 
-
-
-# ============= STABILITY ANALYSIS =============== #
