2619af794a355553e7f2a9ac3d8d98d463b37395
rs2518 <rs2518@ic.ac.uk>
2020-08-17 18:05:14 +0100

    Update paths in load_filetypes.py

--- eda/load_filetypes.py
+++ eda/load_filetypes.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  3 22:03:16 2019
+# =============================================================================
+# TEMPORARY IMPORT FOR USE IN AN INTERPRETER
+# =============================================================================
+import os, sys
 
-@author: raphaelsinclair
-"""
+directory = "Desktop/Term 3 MSc Project"
 
-import pandas as pd
+path = os.path.join(os.path.abspath('.'), directory)
+sys.path.append(path)
+# =============================================================================
+
 import numpy as np
+import pandas as pd
+
+from complex_trait_stats.utils import ROOT
 
 
 
@@ -15,19 +20,29 @@ import numpy as np
 # The following files are the different types of files found for each trait in GeneATLAS
 # NOTE: '*_stats' and 'hla freq' files must be downloaded manually
 
-path = "~/Desktop/MSc Health Data Analytics - IC/HDA/Term 3 MSc Project/Analysis/GeneAtlas/Data/50-0.0/"
+# path = "~/Desktop/MSc Health Data Analytics - IC/HDA/Term 3 MSc Project/Analysis/GeneAtlas/Data/50-0.0/"
+path = ROOT+"/data/results/50-0.0/"
 
-genotyped_res = pd.read_csv(path+"genotyped.allWhites.50-0.0.chr22.csv.gz", sep = " ", compression = 'gzip')
-imputed_res = pd.read_csv(path+"imputed.allWhites.50-0.0.chr22.csv.gz", sep = " ", compression = 'gzip')
+gen_res = pd.read_csv(path+"genotyped.allWhites.50-0.0.chr22.csv.gz",
+                      sep=" ", compression='gzip')
+gen_nr = pd.read_csv(path+"genotyped.normRank.allWhites.50-0.0.chr22.csv.gz",
+                     sep = " ", compression = 'gzip')
+
+imp_res = pd.read_csv(path+"imputed.allWhites.50-0.0.chr22.csv.gz",
+                      sep=" ", compression='gzip')
+imp_nr = pd.read_csv(path+"imputed.normRank.allWhites.50-0.0.chr22.csv.gz",
+                     sep = " ", compression = 'gzip')
+
+
+# gen_stats = pd.read_csv(ROOT+"/data/snps.genotyped.chr22.csv.gz",
+#                         sep=" ", compression='gzip')
+imp_stats = pd.read_csv(ROOT+"/data/snps.imputed.chr22.csv.gz",
+                        sep=" ", compression='gzip')
+
 hla = pd.read_csv(path+"hla.50-0.0.csv.gz", sep = " ", compression = 'gzip')
-hla_omnibus = pd.read_csv(path+"hla.omnibus.50-0.0.csv.gz", sep = " ", compression = 'gzip')
+hla_omnibus = pd.read_csv(path+"hla.omnibus.50-0.0.csv.gz",
+                          sep = " ", compression = 'gzip')
 
-genotyped_nr_res = pd.read_csv(path+"genotyped.normRank.allWhites.50-0.0.chr22.csv.gz", sep = " ", compression = 'gzip')
-imputed_nr_res = pd.read_csv(path+"imputed.normRank.allWhites.50-0.0.chr22.csv.gz", sep = " ", compression = 'gzip')
-
-
-gen_stats = pd.read_csv(path+"snps.genotyped.chr22.csv", sep = " ")
-imp_stats = pd.read_csv(path+"snps.imputed.chr22.csv", sep = " ")
 hla_freq = pd.read_csv(path+"hla.freq", sep = " ")
 
 
