0dc39bad0f7911b4141adf82d56bd4f0153528ef
rs2518 <rs2518@ic.ac.uk>
2020-11-04 09:46:24 +0000

    Remove missing data and split text data into multiple columns

--- preprocess/process_anno.py
+++ preprocess/process_anno.py
@@ -20,8 +20,27 @@ from complex_trait_stats.utils import ROOT
 
 # filepath = os.path.join(ROOT, "data/annovar", "myanno.hg19_multianno.csv")
 # anno = pd.read_csv(filepath)
-
 filepath = os.path.join(ROOT, "data/annovar", "giantanno.hg19_multianno.csv")
 anno = pd.read_csv(filepath)
 
-# ANNOVAR leaves lots of missing data. Consider another source?
+anno.replace({'.':np.nan}, inplace=True)    # Recode "." to np.nan
+
+
+# Drop columns with too much missing data
+threshold = 30
+na_tab = anno.isnull().sum() * 100 / len(anno)
+cols = na_tab[na_tab >= threshold].index.to_list()
+
+# anno.drop(cols, axis=1, inplace=True)
+
+
+# Split 'Gene.refGene' text data into multiple columns
+anno[["refGene1", "refGene2"]] = \
+    anno["Gene.refGene"].str.split(";", expand=True)
+anno.drop(["Gene.refGene"], axis=1, inplace=True)
+anno.replace({None:np.nan}, inplace=True)
+
+
+# Explore missing data in relation to 'Gene.refGene'
+test = anno[not anno["refGene2"].isnull()]
+test2 = anno[anno["refGene2"].isnull() == False]
