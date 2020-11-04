f00cd2b249defa5a33203e5509200f7fbee72cda
rs2518 <rs2518@ic.ac.uk>
2020-11-04 09:57:53 +0000

    Perform model validation on randomly selected sample of data

--- utils.py
+++ utils.py
@@ -17,11 +17,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from scipy.stats import zscore
 
-from sklearn.inspection._permutation_importance import \
-    _calculate_permutation_scores
+# from sklearn.inspection._permutation_importance import \
+#     _calculate_permutation_scores
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import check_scoring
-from sklearn.utils import Bunch
+from sklearn.utils import Bunch, check_random_state
 
 
 
@@ -487,7 +487,46 @@ def _create_control_feature(X, y, positive_control=True, sigma=0.,
         X2[name] = x0
     
     return X2
+       
+
+def _calculate_permutation_scores(estimator, X, y, col_idx,
+                                  random_state, n_repeats, scorer,
+                                  sample_size=0.2):
+    """Calculate score when `col_idx` is permuted
+    
+    Variation of sklearn's '_calculate_permutation_scores'
+    """
+    random_state = check_random_state(random_state)
+
+    # Work on a copy of X to to ensure thread-safety in case of threading based
+    # parallelism. Furthermore, making a copy is also useful when the joblib
+    # backend is 'loky' (default) or the old 'multiprocessing': in those cases,
+    # if X is large it will be automatically be backed by a readonly memory map
+    # (memmap). X.copy() on the other hand is always guaranteed to return a
+    # writable data-structure whose columns can be shuffled inplace.
+    X_permuted = X.copy()
+    scores = np.zeros(n_repeats)
+    shuffling_idx = np.arange(X.shape[0])
+    for n_round in range(n_repeats):
+        random_state.shuffle(shuffling_idx)
+        if hasattr(X_permuted, "iloc"):
+            col = X_permuted.iloc[shuffling_idx, col_idx]
+            col.index = X_permuted.index
+            X_permuted.iloc[:, col_idx] = col
+        else:
+            X_permuted[:, col_idx] = X_permuted[shuffling_idx, col_idx]
         
+        # Take a random sample of the data and associated labels
+        X_sample, _, y_sample, _ = \
+            train_test_split(X_permuted, y, train_size=sample_size,
+                             random_state=random_state)
+        
+        # score on testing
+        feature_score = scorer(estimator, X_sample, y_sample)
+        scores[n_round] = feature_score
+
+    return scores
+
     
 def validate_models(estimators, X, y, scoring=None, n_repeats=5,
                     random_state=None, return_fitted_estimators=False,
@@ -514,10 +553,7 @@ def validate_models(estimators, X, y, scoring=None, n_repeats=5,
     models = []
     estimators = estimators.copy()    # Break links to original list
         
-    for i, estimator in enumerate(estimators):
-
-        # Fit estimator then fit baseline score for comparison
-        
+    for i, estimator in enumerate(estimators):            
         # Get estimator name then calculate permutation importance
         if type(estimator).__name__ == "KerasRegressor":
             models.append("MLP")
